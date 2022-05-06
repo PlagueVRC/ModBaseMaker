be5a3da8c80d492724874d05528dccc3de5d3bbb
MistressPlague <krewellamusicps3@gmail.com>
2022-05-07 00:32:45 +0100

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ModBaseMaker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.FolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox1.Location = new System.Drawing.Point(92, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VRChat Folder:";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(199, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(199, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output Path:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox2.Location = new System.Drawing.Point(92, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(16, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(257, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Build";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mod Name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox3.Location = new System.Drawing.Point(92, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Author Name:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox4.Location = new System.Drawing.Point(92, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(291, 214);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Magenta;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ModBaseMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.FolderBrowserDialog FolderDialog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModBaseMaker.Properties;
+
+namespace ModBaseMaker
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (FolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = FolderDialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (FolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = FolderDialog.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Write ModBase.zip To Output Temporarily
+            WriteResourceToFile("ModBaseMaker.Resources.ModBase.zip", textBox2.Text + "\\ModBase.zip");
+
+            // Create Temp Folder For Editing
+            var TempDir = textBox2.Text + "\\ModBaseTempDir";
+
+            if (!Directory.Exists(TempDir))
+            {
+                Directory.CreateDirectory(TempDir);
+            }
+
+            ZipFile.ExtractToDirectory(textBox2.Text + "\\ModBase.zip", TempDir); // ModBaseTempDir/ModBase/projfiles
+
+            var ProjDir = TempDir + "\\ModBase";
+
+            File.WriteAllText(ProjDir + "\\MainClass.cs",
+                File.ReadAllText(ProjDir + "\\MainClass.cs")
+                .Replace("\"ModBase", $"\"{textBox3.Text}")
+                .Replace("f>ModBase", $"f>{textBox3.Text}")
+                .Replace(@"\\ModBase", $@"\\{textBox3.Text}")
+                .Replace("ModBase", $"{textBox3.Text.Replace(" ", "")}")
+                .Replace("AuthorName", textBox4.Text));
+
+            File.WriteAllText(ProjDir + "\\Misc Classes.cs",
+                File.ReadAllText(ProjDir + "\\Misc Classes.cs")
+                    .Replace("ModBase", $"{textBox3.Text.Replace(" ", "")}"));
+
+            File.WriteAllText(ProjDir + "\\Properties\\AssemblyInfo.cs",
+                File.ReadAllText(ProjDir + "\\Properties\\AssemblyInfo.cs")
+                    .Replace("\"ModBase", $"\"{textBox3.Text}")
+                    .Replace("AuthorName", textBox4.Text));
+
+            var ToVRChatDir = textBox1.Text.EndsWith("/") ? textBox1.Text.Substring(0, textBox1.Text.Length - 1) : textBox1.Text; // To Combat Leading /
+
+            File.WriteAllText(ProjDir + "\\ModBase.csproj",
+                File.ReadAllText(ProjDir + "\\ModBase.csproj")
+                    .Replace("ModBase", $"{textBox3.Text.Replace(" ", "")}")
+                    .Replace("G:\\Games\\SteamLibrary\\steamapps\\common\\VRChat", ToVRChatDir));
+
+            File.Move(ProjDir + "\\ModBase.csproj", ProjDir + $"\\{textBox3.Text.Replace(" ", "")}.csproj");
+
+            File.WriteAllText(ProjDir + "\\ModBase.sln",
+                File.ReadAllText(ProjDir + "\\ModBase.sln")
+                    .Replace("ModBase", $"{textBox3.Text.Replace(" ", "")}"));
+
+            File.Move(ProjDir + "\\ModBase.sln", ProjDir + $"\\{textBox3.Text.Replace(" ", "")}.sln");
+
+            //Cleanup, Finishing Actions
+            Directory.Move(ProjDir, textBox2.Text + $"\\{textBox3.Text.Replace(" ", "")}");
+
+            Directory.Delete(TempDir);
+        }
+
+        public void WriteResourceToFile(string resourceName, string fileName)
+        {
+            using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using (var file = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    resource.CopyTo(file);
+                }
+            }
+        }
+    }
+}
